646900aaf79bfe17f1bdb6d8e74e848de9b4d536
nikitashipkov <71175562+nikitashipkov@users.noreply.github.com>
2023-11-20 13:36:12 +0300

    lab2

--- cvlib/src/select_texture.cpp
+++ cvlib/src/select_texture.cpp
@@ -30,6 +30,16 @@ struct descriptor : public std::vector<double>
         }
         return res;
     }
+
+    double norm_l2() const
+    {
+        double res = 0.0;
+        for (auto v : *this)
+        {
+            res += v * v;
+        }
+        return std::sqrt(res);
+    }
 };
 
 void calculateDescriptor(const cv::Mat& image, int kernel_size, descriptor& descr)
@@ -45,13 +55,16 @@ void calculateDescriptor(const cv::Mat& image, int kernel_size, descriptor& desc
     // \todo implement complete texture segmentation based on Gabor filters
     // (find good combinations for all Gabor's parameters)
     for (auto sig = 5; sig <= 15; sig += 5)
-    {
-        cv::Mat kernel = cv::getGaborKernel(cv::Size(kernel_size, kernel_size), sig, th, lm, gm);
-        cv::filter2D(image, response, CV_32F, kernel);
-        cv::meanStdDev(response, mean, dev);
-        descr.emplace_back(mean.at<double>(0));
-        descr.emplace_back(dev.at<double>(0));
-    }
+        for (auto th = CV_PI / 8; th <= CV_PI; th += CV_PI / 8)
+            for (auto lm = 1; lm <= 15; lm += 1)
+                for (auto gm = 0.1; gm <= 1; gm += 0.1)
+                {
+                    cv::Mat kernel = cv::getGaborKernel(cv::Size(kernel_size, kernel_size), sig, th, lm, gm);
+                    cv::filter2D(image, response, CV_32F, kernel);
+                    cv::meanStdDev(response, mean, dev);
+                    descr.emplace_back(mean.at<double>(0));
+                    descr.emplace_back(dev.at<double>(0));
+                }
 }
 } // namespace
 
@@ -61,7 +74,8 @@ cv::Mat select_texture(const cv::Mat& image, const cv::Rect& roi, double eps)
 {
     cv::Mat imROI = image(roi);
 
-    const int kernel_size = std::min(roi.height, roi.width) / 2; // \todo round to nearest odd
+    int tmp = std::min(roi.height, roi.width) / 2; // \todo round to nearest odd
+    const int kernel_size = (tmp % 2 == 0) ? tmp - 1 : tmp;
 
     descriptor reference;
     calculateDescriptor(image(roi), kernel_size, reference);
@@ -72,15 +86,15 @@ cv::Mat select_texture(const cv::Mat& image, const cv::Rect& roi, double eps)
     cv::Rect baseROI = roi - roi.tl();
 
     // \todo move ROI smoothly pixel-by-pixel
-    for (int i = 0; i < image.size().width / roi.width; ++i)
+    for (int i = 0; i < image.size().width - roi.width; ++i)
     {
-        for (int j = 0; j < image.size().height / roi.height; ++j)
+        for (int j = 0; j < image.size().height - roi.height; ++j)
         {
-            auto curROI = baseROI + cv::Point(roi.width * i, roi.height * j);
+            auto curROI = baseROI + cv::Point(i, j);
             calculateDescriptor(image(curROI), kernel_size, test);
 
             // \todo implement and use norm L2
-            res(curROI) = 255 * ((test - reference).norm_l1() <= eps);
+            res(curROI) = 255 * ((test - reference).norm_l2() <= eps);
         }
     }
 
